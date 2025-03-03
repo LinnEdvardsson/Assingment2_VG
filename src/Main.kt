a895c905fb988d9ea2f7e3de2f2dec9821d3963d
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2025-03-03 21:04:14 +0100

    w

--- src/Main.kt
+++ src/Main.kt
@@ -19,23 +19,22 @@ class SantasHelpers() {
     fun getUnderlings(currentName: String, result: MutableList<String>, santasElfs: MutableMap<String, List<String>>): Any {
         tailrec fun workingElfs(resultList: MutableList<String>, index: Int) {
             if (index == resultList.size) return
-            santasElfs[currentName]?.let { underlings ->
-                resultList.addAll(underlings.filterNot { it in resultList })
-                return workingElfs(resultList, index + 1)
+            santasElfs[currentName]?.let { underlings ->                        // Hitta underlings för currentName från santasElfs
+                resultList.addAll(underlings.filterNot { it in resultList })    // Lägg till alla underlings i resultList, ej dubletter
+                return workingElfs(resultList, index + 1)                 // Fortsätter rekursionen genom att öka index med 1, och gå igenom nästa nästa nyckel i resultList
             }
         }
-        santasElfs[currentName]?.let { result.addAll(it)}
+        santasElfs[currentName]?.let { result.addAll(it)}               // Startar rekursionen, genom att lägga till de direkta underlings för currentName i resultatlistan
         return workingElfs(result, 0)
+
     }
 
 }
-
-
     fun main() {
         val santasHelper = SantasHelpers()
         val santasElfs = santasHelper.createMap()
         val result = mutableListOf<String>()
-        santasHelper.getUnderlings("Butter", result, santasElfs)
+        santasHelper.getUnderlings("Trötter", result, santasElfs)
         println(result)
     }
 
