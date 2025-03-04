026cc427679bcb3590b6d06a842c0d93b42017df
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2025-03-04 14:22:55 +0100

    nicer output

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,22 @@ class SantasHelpers() {
 
     fun createMap(): MutableMap<String, List<String>> {
         val santasElfs: MutableMap<String, List<String>> = mutableMapOf(
-            "Santa" to listOf("Tröger", "Trötter", "Blyger", "Dammråttan", "Skumtomten", "Glader", "Butter", "Rådjuret", "Nyckelpigan", "Haren", "Räven", "Gråsuggan", "Myran", "Bladlusen"),
+            "Santa" to listOf(
+                "Tröger",
+                "Trötter",
+                "Blyger",
+                "Dammråttan",
+                "Skumtomten",
+                "Glader",
+                "Butter",
+                "Rådjuret",
+                "Nyckelpigan",
+                "Haren",
+                "Räven",
+                "Gråsuggan",
+                "Myran",
+                "Bladlusen"
+            ),
             "Glader" to listOf("Tröger", "Trötter", "Blyger"),
             "Butter" to listOf("Rådjuret", "Nyckelpigan", "Haren", "Räven"),
             "Trötter" to listOf("Skummtomten"),
@@ -20,21 +35,23 @@ class SantasHelpers() {
         tailrec fun workingElfs(result: MutableList<String>, index: Int) {
             if (index == result.size) return
             santasElfs[currentName]?.let { underlings ->                        // Hitta underlings för currentName från santasElfs
-                result.addAll(underlings.filterNot { it in result })    // Lägg till alla underlings i result, även underlings till underlings + undvika dubletter.
+                result.addAll(underlings.filterNot { it in result })           // Lägg till alla underlings i result, även underlings till underlings + undvika dubletter.
                 return workingElfs(result, index + 1)                 // Fortsätter rekursionen genom att öka index med 1, och gå igenom nästa nästa nyckel i result
             }
         }
-        santasElfs[currentName]?.let { result.addAll(it)}               // Startar rekursionen, genom att lägga till de direkta underlings för currentName i resultatlistan
+        santasElfs[currentName]?.let { result.addAll(it) }               // Startar rekursionen, genom att lägga till de direkta underlings för currentName i resultatlistan
         return workingElfs(result, 0)
     }
 
 }
+
     fun main() {
         val santasHelper = SantasHelpers()
         val santasElfs = santasHelper.createMap()
         val result = mutableListOf<String>()
-        santasHelper.getUnderlings("Trötter", result, santasElfs)
-        println(result)
+        santasHelper.getUnderlings("Räven", result, santasElfs)
+        result.forEach(::println)
+
     }
 
 
