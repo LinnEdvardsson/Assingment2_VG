26f04741436ff136a289afea95a7d833d4a0a155
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2025-03-05 20:01:02 +0100

    new working function

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,7 @@ class SantasHelpers() {
 
     fun createMap(): MutableMap<String, List<String>> {
         val santasElfs: MutableMap<String, List<String>> = mutableMapOf(
-            "Santa" to listOf("Glader", "Butter"),
+            "Tomten" to listOf("Glader", "Butter"),
             "Glader" to listOf("Tröger", "Trötter", "Blyger"),
             "Butter" to listOf("Rådjuret", "Nyckelpigan", "Haren", "Räven"),
             "Trötter" to listOf("Skummtomten"),
@@ -16,20 +16,19 @@ class SantasHelpers() {
     }
 
 
-    fun getUnderlings(currentName: String, result: MutableList<String>, santasElfs: MutableMap<String, List<String>>): Any {
-        tailrec fun workingElfs(result: MutableList<String>, index: Int) {
-            if (index == result.size) return
-            santasElfs[currentName]?.let { underlings ->                        // Hitta underlings för currentName från santasElfs
-                result.addAll(underlings.filterNot { it in result })           // Lägg till alla underlings i result, även underlings till underlings + undvika dubletter.
-                return workingElfs(result, index + 1)                 // Fortsätter rekursionen genom att öka index med 1, och gå igenom nästa nästa nyckel i result.
+    fun getUnderlings(currentName: String, result: MutableList<String>, santasElfs: MutableMap<String, List<String>>): MutableList<String> {
+        fun getElfs(result: MutableList<String>) : MutableList<String> {
+            santasElfs[currentName]?.let { underlings ->
+                underlings
+                    .forEach { elf ->
+                        result.add(elf)
+                        getUnderlings(elf, result, santasElfs)
+                    }
             }
+            return result
         }
-        santasElfs[currentName]?.let { result.addAll(it) }               // Startar rekursionen, genom att lägga till de direkta underlings för currentName i resultatlistan
-        return workingElfs(result, 0)
+        return getElfs(result)
     }
-
-
-
 }
 
 
@@ -37,7 +36,7 @@ class SantasHelpers() {
         val santasHelper = SantasHelpers()
         val santasElfs = santasHelper.createMap()
         val result = mutableListOf<String>()
-        santasHelper.getUnderlings2("Glader", result, santasElfs)
+        santasHelper.getUnderlings("Tomten", result, santasElfs)
         result.forEach(::println)
     }
 
